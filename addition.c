973e529cbebd7fbb523eb791328d63b20ac03d65
rutujaaaa20 <146871249+rutujaaaa20@users.noreply.github.com>
2023-10-10 13:27:56 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{ 
+	int a,b,sum;
+	
+	printf("enter two numbers");
+	scanf("%d %d",&a,&b);
+	printf("sum=%d",a+b);
+	
+	
+	return 0;
+	
+}
---
+++ programme to print an integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int number;
+	
+	printf("enter any digit");
+	
+	scanf("%d",&number);
+	
+	printf("%d",number);
+	
+	return 0;
+}
